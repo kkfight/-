25e6ea80b7152a12f607e8f1ac1a1a180f45e437
kkfight <763526957@qq.com>
2022-11-23 16:21:30 +0800

    对于FirstCode修改提交

--- FirstCode.c
+++ FirstCode.c
@@ -5,9 +5,13 @@
 
 int main(void)
 {
-	printf("First Output Msg...\n");
+	printf("AAAAAAAAAAAAAAAAAAA\n");
+	printf("AAAAAAAAAAAAAAAAAAA\n");
+	printf("AAAAAAAAAAAAAAAAAAA\n");
+	printf("AAAAAAAAAAAAAAAAAAA\n");
 	while(1)
 		sleep(1)
-return 0;
+        return 0;
 }
+
 
